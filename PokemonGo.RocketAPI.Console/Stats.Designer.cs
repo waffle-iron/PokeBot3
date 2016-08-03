429e9f4430f3315cb97402d00cab40a021b63cdc
ShiftCode <shiftcode@shiftcode.pw>
2016-08-03 17:18:24 +0200

    Small Fixes

--- PokemonGo.RocketAPI.Console/Stats.Designer.cs
+++ PokemonGo.RocketAPI.Console/Stats.Designer.cs
@@ -39,17 +39,9 @@
 			this.xprate = new System.Windows.Forms.Label();
 			this.stardust = new System.Windows.Forms.Label();
 			this.username = new System.Windows.Forms.Label();
-			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.radioButton6 = new System.Windows.Forms.RadioButton();
-			this.radioButton5 = new System.Windows.Forms.RadioButton();
-			this.radioButton3 = new System.Windows.Forms.RadioButton();
-			this.radioButton4 = new System.Windows.Forms.RadioButton();
-			this.radioButton2 = new System.Windows.Forms.RadioButton();
-			this.radioButton1 = new System.Windows.Forms.RadioButton();
 			this.toggleconsole = new System.Windows.Forms.Button();
 			this.togglelist = new System.Windows.Forms.Button();
 			this.groupBox1.SuspendLayout();
-			this.groupBox2.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// groupBox1
@@ -66,14 +58,14 @@
 			this.groupBox1.Controls.Add(this.username);
 			this.groupBox1.Location = new System.Drawing.Point(12, 12);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(260, 291);
+			this.groupBox1.Size = new System.Drawing.Size(260, 267);
 			this.groupBox1.TabIndex = 0;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Stats";
 			// 
 			// levelbar
 			// 
-			this.levelbar.Location = new System.Drawing.Point(126, 262);
+			this.levelbar.Location = new System.Drawing.Point(126, 238);
 			this.levelbar.Name = "levelbar";
 			this.levelbar.Size = new System.Drawing.Size(128, 23);
 			this.levelbar.TabIndex = 9;
@@ -81,7 +73,7 @@
 			// label9
 			// 
 			this.label9.AutoSize = true;
-			this.label9.Location = new System.Drawing.Point(7, 266);
+			this.label9.Location = new System.Drawing.Point(7, 242);
 			this.label9.Name = "label9";
 			this.label9.Size = new System.Drawing.Size(112, 13);
 			this.label9.TabIndex = 8;
@@ -90,7 +82,7 @@
 			// progress
 			// 
 			this.progress.AutoSize = true;
-			this.progress.Location = new System.Drawing.Point(7, 252);
+			this.progress.Location = new System.Drawing.Point(7, 222);
 			this.progress.Name = "progress";
 			this.progress.Size = new System.Drawing.Size(53, 13);
 			this.progress.TabIndex = 7;
@@ -159,105 +151,19 @@
 			this.username.TabIndex = 0;
 			this.username.Text = "User:";
 			// 
-			// groupBox2
-			// 
-			this.groupBox2.Controls.Add(this.radioButton6);
-			this.groupBox2.Controls.Add(this.radioButton5);
-			this.groupBox2.Controls.Add(this.radioButton3);
-			this.groupBox2.Controls.Add(this.radioButton4);
-			this.groupBox2.Controls.Add(this.radioButton2);
-			this.groupBox2.Controls.Add(this.radioButton1);
-			this.groupBox2.Location = new System.Drawing.Point(12, 309);
-			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(260, 157);
-			this.groupBox2.TabIndex = 1;
-			this.groupBox2.TabStop = false;
-			this.groupBox2.Text = "Tasks";
-			// 
-			// radioButton6
-			// 
-			this.radioButton6.AutoSize = true;
-			this.radioButton6.Enabled = false;
-			this.radioButton6.Location = new System.Drawing.Point(7, 133);
-			this.radioButton6.Name = "radioButton6";
-			this.radioButton6.Size = new System.Drawing.Size(42, 17);
-			this.radioButton6.TabIndex = 5;
-			this.radioButton6.TabStop = true;
-			this.radioButton6.Text = "Idle";
-			this.radioButton6.UseVisualStyleBackColor = true;
-			// 
-			// radioButton5
-			// 
-			this.radioButton5.AutoSize = true;
-			this.radioButton5.Enabled = false;
-			this.radioButton5.Location = new System.Drawing.Point(7, 110);
-			this.radioButton5.Name = "radioButton5";
-			this.radioButton5.Size = new System.Drawing.Size(99, 17);
-			this.radioButton5.TabIndex = 4;
-			this.radioButton5.TabStop = true;
-			this.radioButton5.Text = "Softban Bypass";
-			this.radioButton5.UseVisualStyleBackColor = true;
-			// 
-			// radioButton3
-			// 
-			this.radioButton3.AutoSize = true;
-			this.radioButton3.Enabled = false;
-			this.radioButton3.Location = new System.Drawing.Point(7, 64);
-			this.radioButton3.Name = "radioButton3";
-			this.radioButton3.Size = new System.Drawing.Size(112, 17);
-			this.radioButton3.TabIndex = 3;
-			this.radioButton3.TabStop = true;
-			this.radioButton3.Text = "Transfer Pokemon";
-			this.radioButton3.UseVisualStyleBackColor = true;
-			// 
-			// radioButton4
-			// 
-			this.radioButton4.AutoSize = true;
-			this.radioButton4.Enabled = false;
-			this.radioButton4.Location = new System.Drawing.Point(7, 87);
-			this.radioButton4.Name = "radioButton4";
-			this.radioButton4.Size = new System.Drawing.Size(106, 17);
-			this.radioButton4.TabIndex = 2;
-			this.radioButton4.TabStop = true;
-			this.radioButton4.Text = "Evolve Pokemon";
-			this.radioButton4.UseVisualStyleBackColor = true;
-			// 
-			// radioButton2
-			// 
-			this.radioButton2.AutoSize = true;
-			this.radioButton2.Enabled = false;
-			this.radioButton2.Location = new System.Drawing.Point(7, 41);
-			this.radioButton2.Name = "radioButton2";
-			this.radioButton2.Size = new System.Drawing.Size(101, 17);
-			this.radioButton2.TabIndex = 1;
-			this.radioButton2.TabStop = true;
-			this.radioButton2.Text = "Catch Pokemon";
-			this.radioButton2.UseVisualStyleBackColor = true;
-			// 
-			// radioButton1
-			// 
-			this.radioButton1.AutoSize = true;
-			this.radioButton1.Enabled = false;
-			this.radioButton1.Location = new System.Drawing.Point(7, 18);
-			this.radioButton1.Name = "radioButton1";
-			this.radioButton1.Size = new System.Drawing.Size(103, 17);
-			this.radioButton1.TabIndex = 0;
-			this.radioButton1.TabStop = true;
-			this.radioButton1.Text = "Farm PokeStops";
-			this.radioButton1.UseVisualStyleBackColor = true;
-			// 
 			// toggleconsole
 			// 
-			this.toggleconsole.Location = new System.Drawing.Point(8, 472);
+			this.toggleconsole.Location = new System.Drawing.Point(8, 285);
 			this.toggleconsole.Name = "toggleconsole";
 			this.toggleconsole.Size = new System.Drawing.Size(110, 23);
 			this.toggleconsole.TabIndex = 2;
 			this.toggleconsole.Text = "Show/Hide Console";
 			this.toggleconsole.UseVisualStyleBackColor = true;
+			this.toggleconsole.Click += new System.EventHandler(this.toggleconsole_Click);
 			// 
 			// togglelist
 			// 
-			this.togglelist.Location = new System.Drawing.Point(158, 472);
+			this.togglelist.Location = new System.Drawing.Point(158, 285);
 			this.togglelist.Name = "togglelist";
 			this.togglelist.Size = new System.Drawing.Size(114, 23);
 			this.togglelist.TabIndex = 3;
@@ -269,18 +175,16 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 496);
+			this.ClientSize = new System.Drawing.Size(284, 316);
 			this.Controls.Add(this.togglelist);
 			this.Controls.Add(this.toggleconsole);
-			this.Controls.Add(this.groupBox2);
 			this.Controls.Add(this.groupBox1);
 			this.MaximizeBox = false;
 			this.Name = "Stats";
 			this.Text = "Stats";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Stats_FormClosed);
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
-			this.groupBox2.ResumeLayout(false);
-			this.groupBox2.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -288,13 +192,6 @@
 		#endregion
 
 		private System.Windows.Forms.GroupBox groupBox1;
-		private System.Windows.Forms.GroupBox groupBox2;
-		private System.Windows.Forms.RadioButton radioButton3;
-		private System.Windows.Forms.RadioButton radioButton4;
-		private System.Windows.Forms.RadioButton radioButton2;
-		private System.Windows.Forms.RadioButton radioButton1;
-		private System.Windows.Forms.RadioButton radioButton5;
-		private System.Windows.Forms.RadioButton radioButton6;
 		private System.Windows.Forms.ProgressBar levelbar;
 		private System.Windows.Forms.Label label9;
 		private System.Windows.Forms.Label progress;
--- PokemonGo.RocketAPI.Console/Stats.cs
+++ PokemonGo.RocketAPI.Console/Stats.cs
@@ -7,11 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace PokemonGo.RocketAPI.Console
 {
+
 	public partial class Stats : Form
 	{
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GetConsoleWindow();
+
+		[DllImport("user32.dll")]
+		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+		const int SW_HIDE = 0;
+		const int SW_SHOW = 5;
+		bool console = true;
+
 		bool list = false;
 		Pokemons pokemonList = new Pokemons();
 		public Stats()
@@ -20,12 +32,14 @@ namespace PokemonGo.RocketAPI.Console
 			Task.Run(update);
 			if (Globals.pokeList)
 			{
+				list = true;
 				Task.Run(() =>
 				{
-					list = true;
 					pokemonList.ShowDialog();
 				});
 			}
+			else
+				list = false;
 		}
 
 		async Task update()
@@ -62,6 +76,27 @@ namespace PokemonGo.RocketAPI.Console
 					pokemonList.ShowDialog();
 				});
 			}
+		}
+
+		public void toggleconsole_Click(object sender, EventArgs e)
+		{
+			if (console)
+			{
+				var handle = GetConsoleWindow();
+				ShowWindow(handle, SW_HIDE);
+				console = false;
+			}
+			else
+			{
+				var handle = GetConsoleWindow();
+				ShowWindow(handle, SW_SHOW);
+				console = true;
+			}
+		}
+
+		private void Stats_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			System.Environment.Exit(0);
 		}
 	}
 }
